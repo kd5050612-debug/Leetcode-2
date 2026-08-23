7ef78bbc6dcfe6d60a6e41a86261d248e6998364
Krishna Das <kd5050612@gmail.com>
2026-08-23 07:02:30 +0530

    Create Sum Game.java

---
+++ Sum Game.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+        int leftSum = 0;
+        int rightSum = 0;
+        int leftQ = 0;
+        int rightQ = 0;
+        for(int i = 0; i < n/2; i++){
+            if(num.charAt(i) =='?'){
+                leftQ++;
+            }else{
+                leftSum += num.charAt(i) - '0';
+            }
+        }
+        for(int i = n / 2; i < n; i++){
+            if(num.charAt(i)=='?'){
+                rightQ++;
+            }else{
+                rightSum += num.charAt(i) - '0';
+            }
+        }
+        int sumDiff = Math.abs(leftSum - rightSum);
+        int questionDiff = Math.abs(leftQ - rightQ);
+
+        if(questionDiff % 2 == 1){
+            return true;
+        }
+        return sumDiff != 9 * questionDiff / 2;
+    }
+}
