0b6c99e4ca15e6d772578589a68d7c454f58a78b
Krishna Das <kd5050612@gmail.com>
2026-09-16 09:18:50 +0530

    Implement solution for counting non-overlapping segments

---
+++ 1621. Number of Sets of K Non-Overlapping Line Segments.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numberOfSets(int n, int k) {
+        long MOD = 1000000007;
+
+        long[][] dp = new long[n + k][2 * k + 1];
+
+        dp[0][0] = 1;
+
+        for (int i = 1; i < n + k; i++) {
+            dp[i][0] = 1;
+
+            for (int j = 1; j <= 2 * k; j++) {
+                dp[i][j] = (dp[i - 1][j] + dp[i - 1][j - 1]) % MOD;
+            }
+        }
+
+        return (int) dp[n + k - 1][2 * k];
+    }
+}
